9dc7d7664479c74faeae49740d45640f8f215731
Martim Silva <martimtsilva@hotmail.com>
2020-05-14 13:37:40 +0100

    Random dice

--- weblogicmvc/webapp/model/Dice.php
+++ weblogicmvc/webapp/model/Dice.php
@@ -3,7 +3,8 @@ use ActiveRecord\Model;
 
 class Dice extends Model{
     
-    protected function throwDices(){
-
+    public function throwDices(){
+        //Randomizar numero de 1 a 6
+        return rand(1, 6);
     }
 }
