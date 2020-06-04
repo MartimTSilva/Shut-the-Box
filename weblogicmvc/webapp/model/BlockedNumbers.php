a55c10eddaf691eda673625f2b650617a940bc43
Martim Silva <martimtsilva@hotmail.com>
2020-06-04 15:11:09 +0100

    Game update

--- weblogicmvc/webapp/model/BlockedNumbers.php
+++ weblogicmvc/webapp/model/BlockedNumbers.php
@@ -2,8 +2,8 @@
 use ArmoredCore\WebObjects\Session;
 
 class BlockedNumbers{
-    private $_numBlock = [];
-    private $_numBlockCurrentPlay = [];
+    public $_numBlock = [];
+    public $_numBlockCurrentPlay = [];
 
     protected function start(){
         //Coloca vetor a falso
@@ -12,13 +12,13 @@ class BlockedNumbers{
     }
 
     //Recebe os números que quer bloquear
-    protected function blockNumber($number, $diceSum){
+    public function blockNumber($number, $game){
         //Vê se o número já está bloqueado
-        if (in_array($number, $this->_numBlock[])){
+        if (!in_array($number, $this->_numBlockCurrentPlay)){
             //Vê se já tem 2 números bloqueados
             if(sizeof($this->_numBlockCurrentPlay) < 2){
                 //Vê se o numero pressionado é menor ou igual à soma de dados
-                if ($number <= $diceSum){
+                if ($number <= $game->_board->_diceSum){
                     //Adiciona o número aos dos bloqueados
                     array_push($this->_numBlockCurrentPlay, $number);
                     return true;
@@ -26,36 +26,41 @@ class BlockedNumbers{
             }
         } 
         else {
+            //Adiciona o número retirado à soma dos dados
+            $game->_board->_diceSum = $game->_board->_diceSum + $number;
+
             //Desbloqueia o número
             $this->unBlockNumber($number);
             return false;
-        }
-        //Não deixa bloquear o numero
-        return false;        
+        } 
     }
 
     //Recebe os números que quer desbloquear
-    protected function unBlockNumber($number){
+    public function unBlockNumber($number){
         //Retira do array o número que se quer desbloquear
-        array_splice($this->_numBlockCurrentPlay[], $number);
+        if (($key = array_search($number, $this->_numBlockCurrentPlay)) !== false) {
+            unset($this->_numBlockCurrentPlay[$key]);
+        }
     }
 
-    //Vê se a soma dos dados restantes permite bloquear outro número
-    protected function checkPlay($diceSum){
-        if ($diceSum == 0){
-            //Se não houver mais números proveniente da soma de dados, então pode lançar outra vez os dados
-            //Iguala 2 arrays, uma para ficar com os numeros bloqueados definitivos e outros para bloquear os números da jogada
-            array_push($this->_numBlock,  $this->_numBlockCurrentPlay);
+    
+    /*public function checkPlay($game){
+        if ($game->_board->_diceSum == 0){
+            $game->updateGameState(1);
             return true;
         }
         return false;
-    }
+    }*/
 
     //Vai buscar a soma de todos os números bloqueados
-    protected function getFinalPointsSum(){
+    public function getFinalPointsSum(){
         //Vê se é o bot ou o player a jogar e guarda na sessão os números bloquados
-        Session::set('P1_numBlock', $this->_numBlock);
-        Session::set('P2_numBlock', $this->_numBlock);
         return array_sum($this->_numBlock);        
     }
-}
+
+    public function saveBlockedNumbers()
+    {
+        $this->_numBlock = array_merge($this->_numBlock, $this->_numBlockCurrentPlay);
+        $this->_numBlockCurrentPlay = [];
+    }
+}   
--- weblogicmvc/webapp/model/Board.php
+++ weblogicmvc/webapp/model/Board.php
@@ -6,23 +6,25 @@ class Board{
     private $_dice;
     public $_resultDice1;
     public $_resultDice2;
-    public $_blockedNumbersP1 = [];
-    public $_blockedNumbersP2 = [];
+    public $_blockedNumbersP1;
+    public $_blockedNumbersP2;
+    public $_diceSum = 0;
+
+
+    public function __construct(){
+        $this->_blockedNumbersP1 = new BlockedNumbers();
+        $this->_blockedNumbersP2 = new BlockedNumbers();
+    }
 
     //Lança 2 dados
     public function throwDices(){
         $this->_dice = new Dice();
         $this->_resultDice1 = $this->_dice->throwDice();
         $this->_resultDice2 = $this->_dice->throwDice();
-
-        //TODO: Ver se está a jogar o bot ou o player
-        //Board::checkFinalPlayP1($_resultDice1 + $_resultDice2);
-
-        //Else, bot
-        //Board::checkFinalPlayP2($_resultDice1 + $_resultDice2);
+        
+        //Guarda a soma dos dados
+        $this->_diceSum = $this->_resultDice1 + $this->_resultDice2;
     }
-
-
 
     //Verifica se pode jogar mais uma vez
     public function checkFinalPlayP1($sum){
@@ -30,8 +32,7 @@ class Board{
         if($this->checkIfPlayIsPossible($sum)){
             return false;
         } else {
-            //Se não poder, guarda os numeros bloqueados e muda para o bot
-            $this->_blockedNumbersP1 =  BlockedNumbers::$_numBlock;
+            //Se não poder, envia "true"/que é a final play
             return true;
         }
     }
@@ -41,8 +42,7 @@ class Board{
         if($this->checkIfPlayIsPossible($sum)){
             return false;
         } else {
-            //Se não poder, guarda os numeros bloqueados e muda para o bot
-            $this->_blockedNumbersP2 =  BlockedNumbers::$_numBlock;
+            //Se não poder, acaba o jogo
             return true;
         }
     }
@@ -73,24 +73,27 @@ class Board{
 
     public function checkIfPlayIsPossible($sum)
     {
+        $game = Session::get('game');
+
         $_availableNumbers = [1, 2 , 3, 4, 5, 6, 7, 8, 9];
-        $_availableNumbers = array_diff($_availableNumbers, BlockedNumbers::$_numBlock);
-        
+        if ($game->getGameState() == 2){
+            $_availableNumbers = array_diff($_availableNumbers, $game->_board->_blockedNumbersP1->_numBlock);
+        } else if ($game->getGameState() == 4){
+            $_availableNumbers = array_diff($_availableNumbers, $game->_board->_blockedNumbersP2->_numBlock);
+        }
+
         foreach ($_availableNumbers as $number){
             if ($number == $sum){
                 return true;
             } else {
-                foreach ($_availableNumbers as $number_2nd_array)
-                if($number == $number_2nd_array)
-                {
-                    continue;
-                }
-                else
-                {
-                    $num_sum=$number + $number_2nd_array;
-                    if($num_sum == $sum)
-                    {
-                        return true;
+                foreach ($_availableNumbers as $number_2nd_array){
+                    if ($number == $number_2nd_array){
+                        continue;
+                    } else {
+                        $num_sum=$number + $number_2nd_array;
+                        if($num_sum == $sum){
+                            return true;
+                        }
                     }
                 }
             }
